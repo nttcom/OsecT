9cb817576df6bcaf174f1f9f4e2cf2b8c828cf09
TBT0328 <sakra_mochi@yahoo.co.jp>
2021-09-24 18:16:54 +0900

    tsharkからzeekへ変更

--- osect_sensor/Infrastructure/edge_cron/work/ot_tools/bacnet.sh
+++ osect_sensor/Infrastructure/edge_cron/work/ot_tools/bacnet.sh
@@ -4,7 +4,6 @@ cd $1/$2
 mkdir tmp
 cd tmp
 /opt/zeek/bin/zeek /opt/zeek/share/zeek/base/protocols/consts_bacnet.zeek /opt/zeek/share/zeek/base/protocols/main_bacnet.zeek -r $1/$3
-mv bacnet.log bacnet_service.log
 mv bacnet_service.log ../
 cd $1/$2
 rm -rf tmp
