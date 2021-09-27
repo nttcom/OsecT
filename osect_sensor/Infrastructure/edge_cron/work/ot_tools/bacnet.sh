7fb818385716cf28e60acac9c1079aa1fcca59be
TBT0328 <sakra_mochi@yahoo.co.jp>
2021-09-27 14:00:07 +0900

    bacnet.sh修正

--- osect_sensor/Infrastructure/edge_cron/work/ot_tools/bacnet.sh
+++ osect_sensor/Infrastructure/edge_cron/work/ot_tools/bacnet.sh
@@ -3,7 +3,7 @@
 cd $1/$2
 mkdir tmp
 cd tmp
-/opt/zeek/bin/zeek /opt/zeek/share/zeek/base/protocols/consts_bacnet.zeek /opt/zeek/share/zeek/base/protocols/main_bacnet.zeek -r $1/$3
+/opt/zeek/bin/zeek /opt/zeek/share/zeek/base/protocols/consts_bacnet.zeek /opt/zeek/share/zeek/base/protocols/main_bacnet.zeek -r ../../$3
 mv bacnet_service.log ../
-cd $1/$2
+cd ../
 rm -rf tmp
