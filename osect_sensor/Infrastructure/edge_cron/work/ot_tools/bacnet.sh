52c9b35f764c93dbfdfa7368ef32914bd9018420
TBT0328 <sakra_mochi@yahoo.co.jp>
2021-09-28 18:28:20 +0900

    Revert "bacnet.shヘッダ修正"
    This reverts commit 174488f7c3f963e3fb9b07f48e979e9a091e9636.

--- osect_sensor/Infrastructure/edge_cron/work/ot_tools/bacnet.sh
+++ osect_sensor/Infrastructure/edge_cron/work/ot_tools/bacnet.sh
@@ -4,7 +4,6 @@ cd $1/$2
 mkdir tmp
 cd tmp
 /opt/zeek/bin/zeek /opt/zeek/share/zeek/base/protocols/consts_bacnet.zeek /opt/zeek/share/zeek/base/protocols/main_bacnet.zeek -r ../../$3
-sed '/^#/d' bacnet.log > ./bacnet_service.log && sed -i -e "1i #ts     src     dst     resp_p  cmd     apdutype        objtype" bacnet_service.log
 mv bacnet_service.log ../
 cd ../
 rm -rf tmp
