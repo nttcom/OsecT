a9452c84c1a0c5734febe46666b34579ee449403
yutoyamagoshi <y.yamagoshi@ntt.com>
2022-03-04 19:17:04 +0900

    最新版のedge_cronとedge_tcpdumpをOSS版に反映

--- osect_sensor/Application/edge_cron/auth/google_account.py
+++
@@ -1,7 +0,0 @@
-IAP_CLIENT_ID = ''
-""" IAPのCLIENT_IDを指定する """
-OAUTH_TOKEN_URI = 'https://www.googleapis.com/oauth2/v4/token'
-""" OAUTHのURI """
-SERVICE_ACCOUNT_JSON = './auth/service_account.json'
-""" サービスアカウントの鍵情報を指定する """
-
