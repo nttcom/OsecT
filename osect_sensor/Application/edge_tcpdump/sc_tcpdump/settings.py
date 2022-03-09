9ae769f4bc150775a92796d67e7f36e0849827f4
Takuma Tsubaki <tonnkusu+github@gmail.com>
2022-03-09 14:54:49 +0900

    Update settings.py

--- osect_sensor/Application/edge_tcpdump/sc_tcpdump/settings.py
+++ osect_sensor/Application/edge_tcpdump/sc_tcpdump/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "aOhsdUOUOombtmG4D5KJ/RJYfJwPAGyGGo1M11ZtwCbsO9aPyQf4Ic14mefkWthg"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
