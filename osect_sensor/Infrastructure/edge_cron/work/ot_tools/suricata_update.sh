453152df190f075b8c4e17bba50f058aebb2a66a
Kotomi Kuroki <kotomi.kuroki@ntt.com>
2022-03-11 18:09:06 +0900

    Suricata関連修正

--- osect_sensor/Infrastructure/edge_cron/work/ot_tools/suricata_update.sh
+++ osect_sensor/Infrastructure/edge_cron/work/ot_tools/suricata_update.sh
@@ -11,12 +11,14 @@ cd /home/work/
 rm -f $DOWNLOAD_VER_FILE
 wget ${DOWNLOAD_URL_PREFIX}${DOWNLOAD_VER_FILE}
 if [ $? -ne 0 ]; then
+  echo "failed to download ${DOWNLOAD_URL_PREFIX}${DOWNLOAD_VER_FILE}"
   exit 1
 fi
 
 # Compare with current signature version 
 diff current_version $DOWNLOAD_VER_FILE > /dev/null 2>&1
 if [ $? -eq 0 ]; then
+  echo "current version is the latest"
   exit 0
 fi
 
@@ -24,24 +26,32 @@ fi
 rm -f $DOWNLOAD_SIG_FILE
 wget ${DOWNLOAD_URL_PREFIX}${DOWNLOAD_SIG_FILE}
 if [ $? -ne 0 ]; then
+  echo "failed to download ${DOWNLOAD_URL_PREFIX}${DOWNLOAD_SIG_FILE}"
   exit 1
 fi
 
 # Compare MD5 checksum values
 rm -f ${DOWNLOAD_SIG_FILE}.md5
-REMOTE_MD5=`curl ${DOWNLOAD_URL_PREFIX}${DOWNLOAD_SIG_FILE}.md5`
+REMOTE_MD5=`wget -O - ${DOWNLOAD_URL_PREFIX}${DOWNLOAD_SIG_FILE}.md5`
 LOCAL_MD5=`md5sum ${DOWNLOAD_SIG_FILE} | grep -o '^\S*'`
 if [ $REMOTE_MD5 != $LOCAL_MD5 ]; then
+  echo "incorrect md5 value"
   exit 1
 fi
 
 # Extract signature rules
-tar -xzf $DOWNLOAD_SIG_FILE 
-cp -pr rules /var/lib/suricata/
+rm -rf rules
+tar -xzf $DOWNLOAD_SIG_FILE
+rm rules/emerging-ja3.rules
+grep -hv ^# rules/*.rules | grep -v ^$ > /var/lib/suricata/rules/suricata.rules
 mv $DOWNLOAD_VER_FILE current_version
 
 # Set versions
 export SIGNATURE_VERSION=`cat current_version`
 export SURICATA_VERSION=${SURICATA_VERSION}
 
-python3.7 /opt/edge_cron/manage.py send_version
+python3.8 /opt/edge_cron/manage.py send_version
+if [ $? -ne 0 ]; then
+  echo "failed to send version"
+  exit 1
+fi
