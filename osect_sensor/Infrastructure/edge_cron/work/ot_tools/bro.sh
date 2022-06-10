18ac803425297adb955c01da5d5692e78e3f51a6
Takuma Tsubaki <takuma.tsubaki@ntt.com>
2022-06-10 15:55:10 +0900

    zeekで対応可能なプロトコルをインストール

--- osect_sensor/Infrastructure/edge_cron/work/ot_tools/bro.sh
+++ osect_sensor/Infrastructure/edge_cron/work/ot_tools/bro.sh
@@ -3,7 +3,18 @@
 cd $1/$2 || exit
 mkdir tmp
 cd tmp || exit
-/opt/zeek/bin/zeek /opt/zeek/share/zeek/base/protocols/arp.zeek /opt/zeek/share/zeek/base/protocols/ns.zeek /opt/zeek/share/zeek/base/protocols/consts_bacnet.zeek /opt/zeek/share/zeek/base/protocols/main_bacnet.zeek -r $1/$3
+/opt/zeek/bin/zeek -r $1/$3 \
+		   /opt/zeek/share/zeek/base/protocols/arp.zeek \
+		   /opt/zeek/share/zeek/base/protocols/ns.zeek \
+		   /opt/zeek/share/zeek/base/protocols/consts_bacnet.zeek \
+		   /opt/zeek/share/zeek/base/protocols/main_bacnet.zeek \
+		   zeek-plugin-enip \
+		   zeek-plugin-profinet \
+		   zeek-plugin-s7comm \
+		   icsnpp-ethercat \
+		   icsnpp-opcua-binary \
+		   icsnpp-modbus
+# 新しいOTプロトコルに対応する場合は、以下にログを追記
 mv {conn,arp,ns,dns,http}.log ../
 cd $1/$2 || exit
 rm -rf tmp
