1b247571a5a45e4cc37841d00a40cd9a54a964fc
nbhgytzheng <411701076@qq.com>
2022-06-13 16:34:07 +0900

    add switch modbus

--- osect_sensor/Infrastructure/edge_cron/work/ot_tools/bro.sh
+++ osect_sensor/Infrastructure/edge_cron/work/ot_tools/bro.sh
@@ -3,8 +3,11 @@
 cd $1/$2 || exit
 mkdir tmp
 cd tmp || exit
-/opt/zeek/bin/zeek /opt/zeek/share/zeek/base/protocols/arp.zeek /opt/zeek/share/zeek/base/protocols/ns.zeek /opt/zeek/share/zeek/base/protocols/consts_bacnet.zeek /opt/zeek/share/zeek/base/protocols/main_bacnet.zeek -r $1/$3
+/opt/zeek/bin/zeek /opt/zeek/share/zeek/base/protocols/arp.zeek /opt/zeek/share/zeek/base/protocols/ns.zeek /opt/zeek/share/zeek/base/protocols/consts_bacnet.zeek /opt/zeek/share/zeek/base/protocols/main_bacnet.zeek /opt/zeek/share/zeek/site/icsnpp-modbus/main.zeek -r $1/$3
 mv {conn,arp,ns,dns,http}.log ../
+if [ $4 = "True" ]; then
+    mv modbus_detailed.log ../
+fi
 cd $1/$2 || exit
 rm -rf tmp
 # rm packet_filter.log weird.log ssl.log
