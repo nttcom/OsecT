c9a6554bca417b74182cfb8e72cf0bd642454d30
tkhr-ueda <132991586+tkhr-ueda@users.noreply.github.com>
2024-03-21 20:38:48 +0900

    fix: 全角スペースの削除等

--- osect_sensor/sh_tests/ot_tools/yaf/data/gen_pcap.sh
+++ osect_sensor/sh_tests/ot_tools/yaf/data/gen_pcap.sh
@@ -3,7 +3,7 @@
 # 参考: https://tools.netsa.cert.org/yaf/yaf.html
 
 if [[ "$(which tcprewrite)" == "" ]]; then
-    apt-get install -y　tcpreplay
+    apt-get install -y tcpreplay
 fi
 
 # NOTE: seedのプレフィックスが0の場合、pcapが正常に生成されないことがある
